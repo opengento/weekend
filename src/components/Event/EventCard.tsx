7d52b4490c6ca26fc598a2b35e9d197aac2772c4
Thomas Klein <thomasklein876@gmail.com>
2025-09-06 20:26:00 +0200

    Add missing component import

--- src/components/Event/EventCard.tsx
+++ src/components/Event/EventCard.tsx
@@ -7,6 +7,7 @@ import { Event } from "@/interfaces/event";
 import { ButtonLink as ButtonLinkType } from "@/interfaces/link";
 import ButtonLink from "@/components/ButtonLink/ButtonLink";
 import Hero from "@/components/Hero/Hero";
+import Typography from "@/components/Typography/Typography";
 import Time from "@/components/Time/Time";
 
 interface EventCard {
