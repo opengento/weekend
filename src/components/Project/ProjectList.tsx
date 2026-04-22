58e24c9f8226244b625b77be031b734739fcac8e
Thomas KLEIN <tomak@dnd.fr>
2026-04-22 08:42:33 +0200

    Fix build

--- src/components/Project/ProjectList.tsx
+++ src/components/Project/ProjectList.tsx
@@ -1,15 +1,19 @@
 import { ProjectProps } from "@/components/Project/project.types";
 import ProjectCard from "@/components/Project/ProjectCard";
+import { useId } from "react";
 
 interface ProjectListProps {
   projects: ProjectProps[];
 }
 
 const ProjectList = ({ projects }: ProjectListProps) => {
+  const id = useId();
+
   return (
     <div className="flex flex-wrap flex-row md-flex-col gap-8 justify-center align-items">
       {projects.map((project, index) => (
         <ProjectCard
+          key={`${id}-${index}`}
           title={project.title}
           description={project.description}
           tags={project.tags}
